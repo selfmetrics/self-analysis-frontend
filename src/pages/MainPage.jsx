2696ee45be9342cb4adbc94be8319a9cd607c47c
haruna <haruna0306@g.yju.ac.kr>
2026-05-12 12:17:24 +0900

    feature: 메인 페이지 생성

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import EpisodePage from "./EpisodePage";
+
+function MainPage() {
+    return (
+        <div>
+            <h1>自己分析ノート</h1>
+            <EpisodePage>
+
+            </EpisodePage>
+        </div>
+    );
+}
+
+export default MainPage;
